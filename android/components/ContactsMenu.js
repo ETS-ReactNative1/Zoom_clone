71b65bf6fac0029c26b4d5f5ed9cc2c7974a4276
Omkar Acharekar <omkaracharekar12@gmail.com>
2021-11-10 20:39:45 +0530

    Initial commit

---
+++ android/components/ContactsMenu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {View,Text} from "react-native"
+
+function ContactsMenu() {
+    return (
+        <View>
+
+            <Text>Contact</Text>
+
+        </View>
+    )
+}
+
+export default ContactsMenu
